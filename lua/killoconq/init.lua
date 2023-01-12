6ddf8f3583a7ecc0315dac08f5bbee10980f38ee
Fernando Corrales <killoconq@Fer.local>
2023-01-11 18:23:27 -0600

    remap

--- lua/killoconq/init.lua
+++ lua/killoconq/init.lua
@@ -1,2 +1,1 @@
 require('killoconq.remap')
-print('hola from killoconq')
--- lua/killoconq/remap.lua
+++ lua/killoconq/remap.lua
@@ -1,2 +1,14 @@
 vim.g.mapleader = ' '
 vim.keymap.set('n', '<leader>pv', vim.cmd.Ex)
+
+vim.keymap.set('n','<C-d>','<C-d>zz')
+vim.keymap.set('n','<C-u>','<C-u>zz')
+
+vim.keymap.set('n','n','nzzzv')
+vim.keymap.set('n','N','Nzzzv')
+
+-- Exit insert mode
+vim.keymap.set('i','ii','<Esc>')
+
+vim.keymap.set('n','<leader>p',[['_dP']])
+
